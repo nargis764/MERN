b4eca1bb81180022226f2363b48086fae2e6da3b
nargis764 <nargis764@gmail.com>
2022-02-10 12:47:39 -0800

    commit first react

---
+++ React/First_react/app/src/components/List.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const List = () => {
+    //const {margin} = props;
+    return (
+        <div>
+            <ul className="list-margin">
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </div>
+        
+    )    
+}
+
+export default List;
