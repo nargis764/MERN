e93b2a58ce3e33913ce8f2c2de23a2982bf77308
nargis764 <nargis764@gmail.com>
2022-02-10 12:46:34 -0800

    commit first react

---
+++ React/First_react/app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import List from './components/List';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <List />
+    </div>
+  );
+}
+
+export default App;
