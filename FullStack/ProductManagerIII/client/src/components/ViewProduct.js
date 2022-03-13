0f2aaa7ce7eaefbf73b8275a3a6b41d8c8bcbf4a
nargis764 <nargis764@gmail.com>
2022-03-13 12:09:22 -0700

    commit second commit

--- FullStack/ProductManagerIII/client/src/components/ViewProduct.js
+++ FullStack/ProductManagerIII/client/src/components/ViewProduct.js
@@ -1,11 +1,12 @@
 import React,{useState, useEffect} from 'react';
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
     
 
 const ViewProduct = (props) => {
     const [product, setProduct] = useState({});
     const {id} = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get(`http://localhost:8000/api/products/${id}`)
@@ -17,11 +18,23 @@ const ViewProduct = (props) => {
             .catch((err) => console.log(err))
     },[])  
 
+    const deleteProduct = () => {
+        axios.delete(`http://localhost:8000/api/products/${id}`)
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);
+                setProduct("")
+                navigate("/")
+            })
+            .catch((err) => console.log(err))
+    }
+
 return (
     <div className="w-1/2 mx-auto mt-20 leading-10 text-base">
         <p>{product.title}</p>
         <p>{product.price}</p>
         <p>{product.description}</p>
+        <button onClick={deleteProduct}>Delete</button>
     </div>
 )
 }
